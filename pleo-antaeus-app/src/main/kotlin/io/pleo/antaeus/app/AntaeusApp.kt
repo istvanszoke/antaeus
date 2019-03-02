20a79430996e9a2921d84ba87b4bbf23cb835ab8
Istvan Szoke-N <istvan.szoken@gmail.com>
2019-03-02 23:14:15 +0100

    Validating the scheduling logic

--- pleo-antaeus-app/src/main/kotlin/io/pleo/antaeus/app/AntaeusApp.kt
+++ pleo-antaeus-app/src/main/kotlin/io/pleo/antaeus/app/AntaeusApp.kt
@@ -58,6 +58,8 @@ fun main() {
     // This is _your_ billing service to be included where you see fit
     val billingService = BillingService(paymentProvider = paymentProvider)
 
+    billingService.schedulePayment()
+
     // Create REST web service
     AntaeusRest(
         invoiceService = invoiceService,
--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -1,9 +1,25 @@
 package io.pleo.antaeus.core.services
 
 import io.pleo.antaeus.core.external.PaymentProvider
+import java.util.*
+import kotlin.concurrent.schedule
+
 
 class BillingService(
     private val paymentProvider: PaymentProvider
 ) {
-   // TODO - Add code e.g. here
+    private val timer: Timer = Timer("schedule", true)
+
+    fun schedulePayment(): Unit {
+        println("I am scheduled")
+        val scheduledTime = GregorianCalendar.getInstance()
+        //scheduledTime.add(Calendar.MONTH, 1)
+        //scheduledTime.set(Calendar.DAY_OF_MONTH, 1)
+        //scheduledTime.set(Calendar.HOUR_OF_DAY, 8)
+        //scheduledTime.set(Calendar.MINUTE, 0)
+        scheduledTime.add(Calendar.SECOND, 10)
+        timer.schedule(scheduledTime.time) {
+            schedulePayment()
+        }
+   }
 }
